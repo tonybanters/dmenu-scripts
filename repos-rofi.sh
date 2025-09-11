c2f57110a7a3a06f00aace4d65d46cfda7dd83d9
tonybanters <tonybanters@gmail.com>
2025-09-11 09:16:32 -0700

    Added rofi script, and made it more modular for different terminals.

--- repos-rofi.sh
+++ repos-rofi.sh
@@ -7,7 +7,7 @@ terminal="st"
 # Pick repo
 configs="$(ls -1d "$HOME"/repos/*/ 2>/dev/null | xargs -n1 basename)"
 [ -n "$configs" ] || exit 0
-chosen="$(printf '%s\n' $configs | dmenu -i -vi -c -bw 3 -W 600 -l 30 -h 40 -F -fn 'JetBrainsMono Nerd Font:size=16' -p 'Projects:')"
+chosen="$(printf '%s\n' $configs | rofi -dmenu -p 'Projects:')"
 [ -n "$chosen" ] || exit 0
 dir="$HOME/repos/$chosen"
 
@@ -16,5 +16,6 @@ pkill -x $terminal 2>/dev/null || true
 sleep 0.1
 
 # Launch a clean terminal: attach if exists, else create
+# exec st -e tmux new-session -As "$chosen" -c "$dir" "nvim ."
 exec $terminal -e tmux new-session -As "$chosen" -c "$dir"
 
