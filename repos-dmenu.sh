f781b72280afe7308ba728eb96b6251053ea3cf6
tonybtw <tonybtw@tonybtw.com>
2025-09-12 20:44:08 -0700

    fixed readme

--- repos-dmenu.sh
+++ repos-dmenu.sh
@@ -4,10 +4,13 @@ set -eu
 # Set your terminal:
 terminal="st"
 
+# Set your dmenu flags
+dmenu_flags="-i -vi -c -bw 3 -W 600 -l 30 -h 40 -F -fn 'JetBrainsMono Nerd Font:size=16' -p 'Projects:'"
+
 # Pick repo
 configs="$(ls -1d "$HOME"/repos/*/ 2>/dev/null | xargs -n1 basename)"
 [ -n "$configs" ] || exit 0
-chosen="$(printf '%s\n' $configs | dmenu -i -vi -c -bw 3 -W 600 -l 30 -h 40 -F -fn 'JetBrainsMono Nerd Font:size=16' -p 'Projects:')"
+chosen="$(printf '%s\n' $configs | dmenu $dmenu_flags)"
 [ -n "$chosen" ] || exit 0
 dir="$HOME/repos/$chosen"
 
